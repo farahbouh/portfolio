4184020f542bdfa49eaac55c73eb05fa5e048cdf
Farah BOUHNAIDA <farahbouh04@gmail.com>
2026-07-07 02:11:35 +0200

    fix: précompilation du html

--- projets.py
+++ projets.py
@@ -2,21 +2,29 @@ from flask import Flask, render_template
 import json
 import os
 
-app = Flask(__name__, template_folder='.', static_folder='.', static_url_path='')
+app = Flask(__name__, 
+            template_folder='.',      # Cherche les HTML à la racine
+            static_folder='.',        # Cherche CSS/JS à la racine
+            static_url_path='')       # Pas de préfixe /static
 
-
+@app.route('/')
+def accueil():
+    return render_template('index.html')
 
 @app.route('/projets')
 def projets():
     json_path = os.path.join('data', 'projets.json')
-    if not os.path.exists(json_path):
-        return "Erreur : Fichier projets.json manquant", 404
-    
     with open(json_path, encoding='utf-8') as f:
         liste_projets = json.load(f)
     return render_template('projets.html', projets=liste_projets)
 
+@app.route('/apropos')
+def apropos():
+    return render_template('apropos.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
