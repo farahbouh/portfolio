449c2ba6c6e5d86449971f6c54ec56da4498bf3b
Farah BOUHNAIDA <farahbouh04@gmail.com>
2026-07-07 02:01:21 +0200

    fix: correction des chemins

--- projets.py
+++ projets.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template
 import json
+import os
 
-app = Flask(__name__, template_folder='.', static_folder='static', static_url_path='/static')
+app = Flask(__name__, template_folder='.', static_folder='.', static_url_path='')
+
+
 
 @app.route('/projets')
 def projets():
-    with open('data/projets.json', encoding='utf-8') as f:
+    json_path = os.path.join('data', 'projets.json')
+    if not os.path.exists(json_path):
+        return "Erreur : Fichier projets.json manquant", 404
+    
+    with open(json_path, encoding='utf-8') as f:
         liste_projets = json.load(f)
     return render_template('projets.html', projets=liste_projets)
 
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
